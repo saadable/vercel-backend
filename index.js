3fcae3eb9b7c71f45391c1a5927efbafcf59dad5
saadable <139713058+saadable@users.noreply.github.com>
2025-03-18 01:23:48 +0500

    adding

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = 'http://localhost:3000';
+let origin = 'https://vercel-social-media-frontend.vercel.app/';
 console.log('here env', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     origin = process.env.CLIENT_ORIGIN;
